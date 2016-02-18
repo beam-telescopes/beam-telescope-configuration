bb9ef2c37f662112ed16c92b57e554e5f9ad7c37
TCThenna <hendrik.jansen@desy.de>
2016-02-18 17:25:26 +0100

    added NI start scripts, update EUDAQ conf files

---
+++ datura/ni_start_scripts/START_EUDAQ_local.bat
@@ -0,0 +1,22 @@
+taskkill /f /IM  euLog.exe.exe
+taskkill /f /IM  euProd.exe.exe
+taskkill /f /IM  euRun.exe.exe
+taskkill /f /IM  TestLogCollector.exe.exe
+taskkill /f /IM  eulog.exe.exe
+taskkill /f /IM  TestDataCollector.exe.exe
+taskkill /f /IM  NiProducer.exe.exe
+taskkill /f /IM  TLUProducer.exe.exe
+taskkill /f /IM  TestRunControl.exe
+
+set HOSTNAME=192.168.21.2
+set COMPUTERNAME=192.168.21.2
+
+start euRun.exe.exe
+timeout /T 2  > nul
+start TestLogCollector.exe.exe -r tcp://%HOSTNAME%:44000
+timeout /T 2  > nul
+start TestDataCollector.exe.exe  -r tcp://%HOSTNAME%:44000
+timeout /T 2  > nul
+start TLUProducer.exe.exe -r tcp://%HOSTNAME%:44000
+timeout /T 2  > nul
+start NIProducer.exe.exe -r tcp://%HOSTNAME%:44000
