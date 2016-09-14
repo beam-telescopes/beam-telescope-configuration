90e4d19b5b4117b31d7da5434c726fd2c2809d30
dreyling <jan.dreyling-eschweiler@desy.de>
2016-09-14 15:04:29 +0200

    configuration files and network file

---
+++ azalea/ni_start_scripts/START_EUDAQ_local.bat
@@ -0,0 +1,20 @@
+taskkill /f /IM  euRun.exe.exe
+taskkill /f /IM  euLog.exe.exe
+taskkill /f /IM  euProd.exe.exe
+taskkill /f /IM  TestLogCollector.exe.exe
+taskkill /f /IM  TestDataCollector.exe.exe
+taskkill /f /IM  NiProducer.exe.exe
+taskkill /f /IM  TLUProducer.exe.exe
+
+set HOSTNAME=192.168.10.2
+set COMPUTERNAME=192.168.10.2
+
+start euRun.exe.exe
+timeout /T 2  > nul
+start TestLogCollector.exe.exe -r tcp://%HOSTNAME%:44000
+timeout /T 2  > nul
+start TestDataCollector.exe.exe  -r tcp://%HOSTNAME%:44000
+timeout /T 2  > nul
+start TLUProducer.exe.exe -r tcp://%HOSTNAME%:44000
+timeout /T 2  > nul
+start NiProducer.exe.exe -r tcp://%HOSTNAME%:44000
---
+++ azalea/ni_start_scripts/START_EUDAQ_ni_tlu.bat
@@ -0,0 +1,13 @@
+taskkill /f /IM  euRun.exe.exe
+taskkill /f /IM  euLog.exe.exe
+taskkill /f /IM  euProd.exe.exe
+taskkill /f /IM  TestLogCollector.exe.exe
+taskkill /f /IM  TestDataCollector.exe.exe
+taskkill /f /IM  NiProducer.exe.exe
+taskkill /f /IM  TLUProducer.exe.exe
+
+set HOSTNAME=192.168.10.2
+set RUNCONTROL=192.168.10.1
+
+start NiProducer.exe.exe -r tcp://%RUNCONTROL%:44000
+start TLUProducer.exe.exe -r tcp://%RUNCONTROL%:44000
