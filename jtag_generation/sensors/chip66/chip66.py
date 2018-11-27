5e6a460ce1b3018a2fe456255b29d03ce387d86a
Jan Dreyling-Eschweiler <jan.dreyling-eschweiler@desy.de>
2018-11-27 11:35:11 +0100

    duranta with 1b, prepare sensor 66

--- jtag_generation/sensors/chip66/chip66.py
+++ jtag_generation/sensors/chip66/chip66.py
@@ -1,20 +1,39 @@
 ######################################
-# Last update: 29 August 2016, Jan Dreyling-Eschweiler
+# Last update: 27 November 2018, Jan Dreyling-Eschweiler
 
 ######################################
 # Sensor Name
 sensor_name = "66"
-# Input names
-input_folder = "former2015/"
-input_file_name = "chip66_thresh"
+
+# Middlepoints in DAC
+IVDREF2  = 100
+IVDREF1A = 127
+IVDREF1B = 115
+IVDREF1C = 132
+IVDREF1D = 94
+# Temporal noise: TN
+THN_matA = 1.037
+THN_matB = 0.9707
+THN_matC = 1.006
+THN_matD = 0.9992
+# Fixed pattern noise: FPN
+FPN_matA = 0.4316
+FPN_matB = 0.4338
+FPN_matC = 0.484
+FPN_matD = 0.5028
+# Offset
+OFF_matA = 0.2222
+OFF_matB = 0.2318
+OFF_matC = -0.1147
+OFF_matD = 0.4469
 # columns to disable, if as a list 
 DIS_col = []
 
 ######################################
 # Generate using Jtag class
-from generate_jtag import Jtag_update
+from generate_jtag import Jtag
 # Constructor
-chip = Jtag_update(sensor_name, input_folder, input_file_name, DIS_col)
+chip = Jtag(sensor_name, IVDREF2, IVDREF1A, IVDREF1B, IVDREF1C, IVDREF1D, THN_matA, THN_matB, THN_matC, THN_matD, FPN_matA, FPN_matB, FPN_matC, FPN_matD, OFF_matA, OFF_matB, OFF_matC, OFF_matD, DIS_col)
 # Print values
 chip.values()
 # Generate txt-files from threshold minimum to maximum
---
+++ jtag_generation/sensors/chip66/chip66_old.py
@@ -0,0 +1,21 @@
+######################################
+# Last update: 29 August 2016, Jan Dreyling-Eschweiler
+
+######################################
+# Sensor Name
+sensor_name = "66"
+# Input names
+input_folder = "former2015/"
+input_file_name = "chip66_thresh"
+# columns to disable, if as a list 
+DIS_col = []
+
+######################################
+# Generate using Jtag class
+from generate_jtag import Jtag_update
+# Constructor
+chip = Jtag_update(sensor_name, input_folder, input_file_name, DIS_col)
+# Print values
+chip.values()
+# Generate txt-files from threshold minimum to maximum
+chip.generate(3, 12)
